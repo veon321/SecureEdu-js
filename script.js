7512d44baa07cd169ceec852e292cc8f994d633d
unknown <veon@vp.pl>
2026-07-03 18:09:35 +0200

    SecureEdu

---
+++ script.js
@@ -0,0 +1,52 @@
+const users = [
+  { id: 1, name: "Jan Kowalski", role: "admin" },
+  { id: 2, name: "Anna Nowak", role: "teacher" },
+  { id: 3, name: "Piotr Gryz", role: "student", grades: [3, 4, 5] },
+  { id: 4, name: "Marta Żak", role: "guest" },
+];
+
+const startButtons = document.querySelectorAll(".user");
+const menuContainer = document.getElementById("menu-container");
+
+const showPanel = function (event) {
+  const user = event.target.classList[1]; // np. "admin"
+  const panel = document.querySelector(`.panel.${user}`);
+
+  panel.classList.remove("hidden");
+  menuContainer.classList.add("hidden");
+
+  const runRoleMechanics = roleRegistry[user];
+
+  if (runRoleMechanics) {
+    runRoleMechanics(user);
+  } else {
+    console.error(`Nie znaleziono mechanik dla roli: ${user}`);
+  }
+};
+
+startButtons.forEach((button) => {
+  button.addEventListener("click", showPanel);
+});
+
+const admin = function (user) {
+  console.log(user);
+};
+
+const teacher = function (user) {
+  console.log(user);
+};
+
+const student = function (user) {
+  console.log(user);
+};
+
+const guest = function (user) {
+  console.log(user);
+};
+
+const roleRegistry = {
+  admin: admin,
+  teacher: teacher,
+  student: student,
+  guest: guest,
+};
